a4aa601d44fad84b17ee05d9aa421214e51ebbd7
SomeKath <torresmario97@outlook.com>
2019-04-01 11:39:56 -0500

    Creada clase if

--- src/interfaz/Principal.java
+++ src/interfaz/Principal.java
@@ -6,6 +6,7 @@
 package interfaz;
 
 import logica.CapturaTeclado;
+import logica.EjemploCondicional;
 
 /**
  *
@@ -16,7 +17,11 @@ public class Principal {
     public static void main(String[] args){
         
         CapturaTeclado ct = new CapturaTeclado();
+        System.out.println("Ejemplo de captura. ");
         ct.operacion();
+        EjemploCondicional ec = new EjemploCondicional();
+        System.out.println("Ejemplo if. ");
+        ec.operacion();
         
     }
     
