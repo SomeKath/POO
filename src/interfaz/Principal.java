c14bd63d5f3da7a68ffcfee36f8b4f8287ebbaf0
Maeio Torres <torresmario97@outlook.com>
2019-04-02 23:43:17 -0500

    Borradas plantilla en el main
    Prueba de orden de commits y GitHub escritorio

--- src/interfaz/Principal.java
+++ src/interfaz/Principal.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package interfaz;
 
 import logica.CapturaTeclado;
 import logica.EjemploCiclo;
 import logica.EjemploCondicional;
 
-/**
- *
- * @author Estudiantes
- */
 public class Principal {
     
     public static void main(String[] args){
